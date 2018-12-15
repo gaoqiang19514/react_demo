91ea79f0d9627144a788b876d9a0b8c21ab40e8b
gaoqiang19514 <gaoqiang19514@163.com>
2018-12-15 18:38:43 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-- [ ] 加入redux react-redux
-- [ ] 为什么PrivateRoute组件只执行了一次？
+- [x] 加入redux react-redux
+- [x] 为什么PrivateRoute组件只执行了一次？
+        使用了connect的路由需要用withRouter包装一遍
 - [ ] 加入路由过渡淡入淡出
 - [ ] 以上对路由的淡出淡出可能会与rem冲突 需解决
--- src/App.js
+++ src/App.js
@@ -2,33 +2,54 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  NavLink
+  NavLink,
+  Switch
 } from "react-router-dom";
 import './App.css';
 
 import Auth from './components/Auth';
-import Login from './components/Login'
+import Login from './components/Login';
+
+const duration = 300;
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+};
+
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered:  { opacity: 1 },
+};
 
 const Public = () => <h1>Public</h1>;
 const Protected = () => <h1>Protected</h1>;
+const NotFound = () => <h1>Not Found</h1>;
 
 class App extends Component {
     render() {
         return (
             <Router>
-                <div>
-                    <nav>
-                        <NavLink to="/public">Public Page</NavLink>
-                        <NavLink to="/protected">Protected Page</NavLink>
-                    </nav>
-
-                    <main>
-                        <Route path="/public" component={Public} />
-                        <Auth path="/protected" component={Protected} />
-
-                        <Route path="/login" component={Login} />
-                    </main>
-                </div>
+                <Route
+                    render={ ({ location }) => {
+                        return  (
+                            <div>
+                                <nav>
+                                    <NavLink to="/public">Public Page</NavLink>
+                                    <NavLink to="/protected">Protected Page</NavLink>
+                                </nav>
+                                <main>
+                                    <Switch>
+                                        <Route path="/public" component={Public} />
+                                        <Auth path="/protected" component={Protected} />
+                                        <Route path="/login" component={Login} />
+                                        <Route render={ () => <NotFound /> } />
+                                    </Switch>
+                                </main>
+                            </div>
+                        )
+                    }}
+                />
             </Router>
         );
     }
