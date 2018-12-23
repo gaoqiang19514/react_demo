48d89b397f3b79a192c286b27bc322f9702634e9
gaoqiang19514 <gaoqiang19514@163.com>
2018-12-23 13:36:01 +0800

    update

--- src/Store.js
+++ src/Store.js
@@ -1,44 +1,40 @@
 import { createStore } from 'redux';
 
 const initialState = {
-    loading: false,
     token: '',
+    refresh_token: '',
     isAuthenticated: false
 };
 
+const saveToken = (payload) => {
+    localStorage.setItem('token', payload.token);
+    localStorage.setItem('refresh_token', payload.refresh_token);
+};
+
+const removeToken = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('refresh_token');
+};
+
 const reducer = (state = initialState, action) => {
-    if(action.type === 'SHOW_LOADING'){
-        return {
-            ...state,
-            loading: true
-        }
+    switch(action.type){
+        case 'AUTH_SUCCESS':
+            saveToken(action.payload);
+            return {
+                ...state,
+                token: action.payload.token,
+                refresh_token: action.payload.refresh_token,
+                isAuthenticated: true
+            }
+        case 'UNAUTH_USER':
+            removeToken();
+            return {
+                ...state,
+                token: '',
+                refresh_token: '',
+                isAuthenticated: false
+            }
     }
-
-    if(action.type === 'HIDE_LOADING'){
-        return {
-            ...state,
-            loading: false
-        }
-    }
-
-    if(action.type === 'AUTH_SUCCESS'){
-        localStorage.setItem('token', action.payload.token);
-        return {
-            ...state,
-            token: action.payload.token,
-            isAuthenticated: true
-        }
-    }
-
-    if(action.type === 'UNAUTH_USER'){
-        localStorage.removeItem('token');
-        return {
-            ...state,
-            token: '',
-            isAuthenticated: false
-        }
-    }
-
 
     return state;
 }
