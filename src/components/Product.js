6fb4c6df608935dca7c7eb3ff20209e75000fb98
gaoqiang19514 <gaoqiang19514@163.com>
2019-01-24 18:15:41 +0800

    update add product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,2 +1,28 @@
 import React, { Component } from 'react'
+import { Link, Route } from "react-router-dom"
 import styled from 'styled-components'
+
+const Modal = () => {
+  const fixedStyle = {
+
+  }
+  return (
+    <div style={fixedStyle}>
+      Modal
+    </div>
+  )
+}
+
+class Product extends Component {
+
+  render() {
+    return (
+      <div>
+        <Link to='/product/layer'>layer</Link>
+        <Route path="/product/layer" component={Modal} />
+      </div>
+    )
+  }
+}
+
+export default Product
