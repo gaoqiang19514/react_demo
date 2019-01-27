d6943c489bfb98d87e2b899e7a4183031a316f3e
gaoqiang19514 <gaoqiang19514@163.com>
2019-01-27 14:49:35 +0800

    update

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,41 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
 
-const title = 'React Dependency Injection'
-// 将Injector组件的this.props this.state以参数的形式传入Part组件
-const inject = function(Part) {
-  return class Injector extends Component {
-    render() {
-      return (
-        <Part
-          { ...this.state }
-          { ...this.props }
-          title={ title }
-        />
-      )
-    }
-  }
-}
-
-const Title = function({ title }) {
-  return <h1>{ title }</h1>
-}
-
-var EnhancedTitle = inject(Title);
-const Header = function({ title }) {
-  return (
-    <header>
-      <EnhancedTitle />
-    </header>
-  )
-}
-
-export default class extends Component {
-  render() {
-    return (
-      <div>
-        <Header/>
-      </div>
-    )
-  }
+export default function(){
+  return null
 }
