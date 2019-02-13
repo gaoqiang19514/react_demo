6e17dde17304b77ee8f79cd9309aee1e6eb697e9
gaoqiang19514 <gaoqiang19514@163.com>
2019-02-13 17:38:36 +0800

    update

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
-import styled from 'styled-components'
+
+const withInnerWidth = Part => {
+  return class extends Component {
+    state = {innerWidth: window.innerWidth}
+
+    componentDidMount() {
+      window.addEventListener('resize', this.handleResize)
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener('resize', this.handleResize)
+    }
+
+    handleResize() {
+      this.setState({innerWidth: window.innerWidth})
+    }
+
+    render() {
+      return <Part {...this.props} {...this.state} />
+    }
+  }
+}
+
+const MyComponent = ({innerWidth}) => {
+  return <div>MyComponent {innerWidth}</div>
+}
+
+const MyComponentWith = withInnerWidth(MyComponent)
 
 export default function(){
-  return null
+  return <MyComponentWith />
 }
