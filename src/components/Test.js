9ef49a594f044a3beb0139896e79f04133db4e44
gaoqiang19514 <gaoqiang19514@163.com>
2019-02-13 18:44:13 +0800

    update

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { flattenProp, renameProp, compose } from 'recompose'
 
 const withInnerWidth = Part => {
   return class extends Component {
@@ -28,6 +30,46 @@ const MyComponent = ({innerWidth}) => {
 
 const MyComponentWith = withInnerWidth(MyComponent)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Profile = ({name, age, innerWidth}) => {
+  return (
+    <div>
+      <div>name: {name}</div>
+      <div>Age: {age}</div> 
+      <div>innerWidth: {innerWidth}</div>
+    </div>
+  )
+}
+
+Profile.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number
+}
+
+const enhance = compose(
+  flattenProp('user'),
+  renameProp('username', 'name'),
+  withInnerWidth
+)
+
+// 向ProfileWithFlattenUser传入一个user对象 user会被flattenProp展开为扁平的username和age
+const ProfileWithFlattenUser = enhance(Profile) 
+
 export default function(){
-  return <MyComponentWith />
+  return <ProfileWithFlattenUser user={{username: 'tom', age: 28}} />
 }
