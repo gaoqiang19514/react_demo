23a252bd2f244af41d4750084938541f2012ae5b
gaoqiang19514 <gaoqiang19514@163.com>
2019-03-05 14:15:07 +0800

    update

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,111 +1,53 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import { Transition } from 'react-transition-group'
+import uuid from 'uuid'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
+import styled from 'styled-components'
+const Random = require('mockjs').Random
 
-const Portal = (props) => {
-  return ReactDOM.createPortal(
-    props.children,
-    props.node
-  )
-}
+const StyledItem = styled.div`
+  margin: 15px;
+  padding: 15px;
+  border: 1px solid #ccc;
+  background: #fff;
+`
 
-const defaultProps = {
-  unmountOnClose: true,
-  zIndex: 1050
-}
-
-class Modal extends Component {
+export default class Test extends Component {
   constructor(props) {
     super(props)
 
-    this._element = null
-    this.onClosed = this.onClosed.bind(this)
+    this.state = {show: false, items: [
+      {id: uuid(), title: Random.paragraph(1, 2)},
+      {id: uuid(), title: Random.paragraph(1, 2)},
+      {id: uuid(), title: Random.paragraph(1, 2)},
+      {id: uuid(), title: Random.paragraph(1, 2)},
+      {id: uuid(), title: Random.paragraph(1, 2)},
+      {id: uuid(), title: Random.paragraph(1, 2)},
+      {id: uuid(), title: Random.paragraph(1, 2)}
+    ]}
 
-    this.state = {isOpen: props.isOpen}
-
-    if(props.isOpen) {
-      this.init()
-    }
+    this.toggle = this.toggle.bind(this)
+    this.removeById = this.removeById.bind(this)
   }
 
-  componentWillReceiveProps(nextProps) {
-    if(nextProps.isOpen && !this.state.isOpen) {
-      this.setState({isOpen: nextProps.isOpen})
-    }
+  toggle() {
+    this.setState({show: !this.state.show})
   }
 
-  componentWillUpdate(nextProps, nextState) {
-    if(nextState.isOpen && !this.state.isOpen) {
-      this.init()
-    }
-  }
-  
-  init() {
-    if(!this._element) {
-      this._element = document.createElement('div')
-      this._element.setAttribute('tabindex', '-1')
-      this._element.style.position = 'relative'
-      this._element.style.zIndex = this.props.zIndex
-      document.body.appendChild(this._element)
-    }    
-  }
-
-  destroy() {
-    if(this._element) {
-      document.body.removeChild(this._element)
-      this._element = null
-    }
-  }
-
-  onClosed() {
-
-    if(this.props.unmountOnClose) {
-      this.destroy()
-    }
-
-    this.setState({isOpen: false})
+  removeById(id) {
+    const items = this.state.items.filter(item => item.id !== id)
+    this.setState({items: items})
   }
 
   render() {
-    if(this.state.isOpen) {
-      return (
-        <Portal node={this._element}>
-          <Transition
-            in={this.props.isOpen}
-            timeout={300}
-            onExited={this.onClosed}
-          >
-            {(state) => {
-              return (
-                <div>{this.props.children}</div>
-              )
-            }}
-          </Transition>
-        </Portal>
-      )
-    }
-
-    return null
-  }
-}
-
-Modal.defaultProps = defaultProps
-
-export default class Test extends Component {
-  state = {isOpen: false}
-
-  toggle = () => {
-    this.setState({isOpen: !this.state.isOpen})
-  }
-
-  render() {
+    const {items} = this.state
     return (
-      <div>
-        <button onClick={this.toggle}>toggle</button>
-        <Modal isOpen={this.state.isOpen}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda nesciunt exercitationem explicabo non veniam? Dolorum voluptatem distinctio sed, necessitatibus laudantium pariatur error dolore iste nam autem quis aspernatur sit esse.
-        </Modal>
-      </div>
+      <TransitionGroup>
+        {items.map(item => {
+          return <CSSTransition key={item.id} timeout={500} classNames="fade">
+            <StyledItem className="item" id={item.id}>{item.title}<button onClick={() => this.removeById(item.id)}>删除</button></StyledItem>
+          </CSSTransition>
+        })}
+      </TransitionGroup>
     )
   }
 }
